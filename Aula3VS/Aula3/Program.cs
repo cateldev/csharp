c95ba8e583e119def8755c3cfdb667fcb9d4ebff
cateldev <catelmatt@hotmail.com>
2022-04-04 11:56:20 -0300

    @cateldev [Aula: 3]

---
+++ Aula3VS/Aula3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Entre com a sua idade");
+            int age = Convert.ToInt32((string)Console.ReadLine());
+
+            Console.WriteLine("Oi " + name + "\nQue legal que voce tem " + age + " anos de idade");
+
+            Console.WriteLine("Qual seu filme favorito?");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula3VSC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula3VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float height;
+            float weight;
+            float imc;
+
+            Console.WriteLine("Digite seu nome: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Olá, " + name + "! Digite sua altura: ");
+            height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu peso: ");
+            weight = float.Parse(Console.ReadLine());
+
+            imc = weight / (height * height);
+
+            Console.WriteLine ("Seu IMC é " + imc + ". Ta gordo pa caralho ein, filho da puta!");
+        }
+    }
+}
