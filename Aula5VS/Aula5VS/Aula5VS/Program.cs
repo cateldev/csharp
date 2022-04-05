5e6f78921c779dcdea135113363945013fd5662d
cateldev <catelmatt@hotmail.com>
2022-04-05 12:49:55 -0300

    @cateldev Exercices Class 5

---
+++ Aula5VS/Aula5VS/Aula5VS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula5VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 5;
+
+            if (valor == 5)
+            {
+                Console.WriteLine("O valor é 5");
+            }
+            else
+            {
+                Console.WriteLine("Não é o valor 5");
+            }
+        }
+    }
+}
