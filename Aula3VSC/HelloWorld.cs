c0db1f484739f36348156d3057b04b3eb35c16a5
cateldev <catelmatt@hotmail.com>
2022-04-05 08:22:58 -0300

    @cateldev Exercice Class 3

---
+++ Aula3VSC/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+    static void main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+        Console.ReadLine();
+    }
+    }
+}
