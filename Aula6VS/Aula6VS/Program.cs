f11616de037a894ae4c7f52c10246616b0846fc9
cateldev <catelmatt@hotmail.com>
2022-04-06 12:48:00 -0300

    @cateldev Class 6

---
+++ Aula6VS/Aula6VS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula6VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            for(int i = 1; i < 20; i++)
+            {
+                Console.WriteLine($"Posição {i} Valor que deseja somar: ");
+                valor += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Somatória: {valor}");
+        }
+    }
+}
---
+++ Aula6VSC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula6VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada");
+
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    //Console.WriteLine(x + "x" + y + "=" + x * y);
+                    Console.WriteLine ($"{b} x {p} = {b*p}");
+                }
+            }
+
+            
+        }
+    }
+}
